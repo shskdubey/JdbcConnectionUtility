d2199dcd65cd5b46a05a42dce0992c587978cdf1
shskdubey <Kartikey.Dubey@transformco.com>
2020-10-16 23:28:08 +0530

    Improved commands

--- src/main/java/com/searshc/hs/pws/MainTrigger.java
+++ src/main/java/com/searshc/hs/pws/MainTrigger.java
@@ -17,15 +17,15 @@ public class MainTrigger {
 		if (arguments != null && arguments.length>=4) {
 			LOGGER.info("Using following arguments for the jdbc connection : ");
 			
-			if(arguments[0].equalsIgnoreCase("-v") && arguments.length >= 4 ) {
-				LOGGER.info("\n url= {} \n userName={} \n password ={} \n ",arguments[2],arguments[3],arguments[4]);
-				JdbcConnectionCmdLine.jdbcConnectionCmdCreater(JDBCDriverFilter.getDriverClass(arguments[1]), arguments[2],
-						arguments[3], arguments[4]);
+			if(arguments[0].equalsIgnoreCase("-v") && arguments.length >= 5 ) {
+				LOGGER.info("\n url= {} \n userName={} \n password ={} \n sqlQueries= {} \n",arguments[1],arguments[2],arguments[3],Arrays.toString(getArrayOfSqlQueries(arguments)));
+				CreateJdbcConnection.jdbcConnectionTester(JDBCDriverFilter.getDriverClass(arguments[1]), arguments[2],
+						arguments[3], arguments[4], getArrayOfSqlQueries(arguments));
 			}
 			else {
-				LOGGER.info("\n url= {} \n userName={} \n password ={} \n sqlQueries= {} \n",arguments[1],arguments[2],arguments[3],Arrays.toString(getArrayOfSqlQueries(arguments)));
-				CreateJdbcConnection.jdbcConnectionTester(JDBCDriverFilter.getDriverClass(arguments[0]), arguments[1],
-						arguments[2], arguments[3], getArrayOfSqlQueries(arguments));
+				LOGGER.info("\n url= {} \n userName={} \n password ={} \n ",arguments[1],arguments[2],arguments[3]);
+				JdbcConnectionCmdLine.jdbcConnectionCmdCreater(JDBCDriverFilter.getDriverClass(arguments[0]), arguments[1],
+						arguments[2], arguments[3]);
 			}
 		}
 		else {
@@ -35,6 +35,6 @@ public class MainTrigger {
 	}
 
 	public static String[] getArrayOfSqlQueries(String args[]) {
-		return Arrays.copyOfRange(args, 4, args.length);
+		return Arrays.copyOfRange(args, 5, args.length);
 	}
 }
